32d14ef7e3ab006fbadd35327c361c6ef13d7e6c
YukiSato <yukiyukiamam@gmail.com>
2018-12-14 22:35:48 +0900

    top-page-complete

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,8 @@
 class HomeController < ApplicationController
+  def top
+    @user = User.new
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  #################### home #######################
+  get '/', to: 'home#top'
+  #################################################
+  #get '/signup', to: 'users#new'
+  post '/', to: 'users#create'
+  ##################  Session  ####################
+  #get '/login', to:'sessions#new'
+  patch '/', to:'sessions#create' #patchにしてるのはどうしても'/'が使いたいから
+  delete '/logout', to:'sessions#destroy'
+  ##################  users  #########################
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
