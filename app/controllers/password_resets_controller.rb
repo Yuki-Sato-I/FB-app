280d52d8323c74a7d22dcfe992e792023f78601d
YukiSato <yukiyukiamam@gmail.com>
2018-12-26 10:32:35 +0900

    complete password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -39,6 +39,7 @@ class PasswordResetsController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:password,:password_confirmation)
+    end
 
     def get_user
       @user = User.find_by(email: params[:email])
