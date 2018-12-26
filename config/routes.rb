a593652b985163fa9f60e9ce81134c6c5e7d6e8d
YukiSato <yukiyukiamam@gmail.com>
2018-12-26 10:19:15 +0900

    complete password

--- config/routes.rb
+++ config/routes.rb
@@ -31,5 +31,7 @@ Rails.application.routes.draw do
   resources :users
   ###################  mail  ######################
   resources :account_activations, only:[:edit]
+  ##################  password_reset  ###############
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
